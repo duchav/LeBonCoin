529651d0637809a02d659d5cd57dc8d0edccb56d
Philippe <Philippe@NewAirdPhilippe.lan>
2025-03-08 18:14:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+   
+  </header>
+<p>Hello world</p>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
